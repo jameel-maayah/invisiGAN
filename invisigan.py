277d5f2844a3743e147f8e5da741e6e695119a6a
Maayah, Jameel Kais <jmaayah3@gatech.edu>
2025-10-04 20:56:50 -0400

    Update invisigan.py
    No one liners

--- invisigan.py
+++ invisigan.py
@@ -40,7 +40,13 @@ ANIMATION_DIR = "invisigan_generated_images"
 CHECKPOINT_DIR = "invisigan_model_checkpoints"
 
 # MPS backend
-device = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+elif torch.backends.mps.is_available():
+    device = torch.device("mps")
+else:
+    device = torch.device("cpu")
+    
 print(f"Using device: {device}")
 
 # Data preprocessing
